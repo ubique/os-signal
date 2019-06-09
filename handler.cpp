ec6d456bb42328a3a28f073cb2e8e7755789f4c0
Daniel Oreshnikov <dan.io.oreshnikov@gmail.com>
2019-06-09 10:45:11 +0300

    Basic functions: - need to remove main.cpp from CMakeLists + basic constructor and definitions

---
+++ handler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dan on 6/9/19.
+//
+
+#include "handler.hpp"
---
+++ handler.hpp
@@ -0,0 +1,16 @@
+//
+// Created by dan on 6/9/19.
+//
+
+#ifndef PROJECT_HANDLER_HPP
+#define PROJECT_HANDLER_HPP
+
+
+
+class handler {
+
+};
+
+
+
+#endif //PROJECT_HANDLER_HPP
