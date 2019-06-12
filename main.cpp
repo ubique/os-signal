d4fa8b46652467ce26a8f728c162513d7123aeea
Roman Smirnov <smirnovromal@yandex.ru>
2019-06-12 22:17:34 +0300

    fixed signal safety

--- main.cpp
+++ main.cpp
@@ -16,13 +16,33 @@ using std::cerr;
 using std::endl;
 using std::string;
 
-
-gregset_t *gregset;
+template<typename T>
+void print_hex_num(T num, int len) {
+    write(STDOUT_FILENO, "0x", 2);
+    uint8_t ones = 15;
+    for (int i = 0; i < len; ++i) {
+        char show = (num >> (len - 1 - i) * 4) & ones;
+        show += show < 10 ? '0' : 'A' - 10;
+        write(STDOUT_FILENO, &show, 1);
+    }
+}
 
 template<typename T>
-void print_register(const string &name, T reg) {
-    cout << name << ": " << "0x" << std::hex
-         << std::setfill('0') << std::setw(sizeof(void *) * 2) << (*gregset)[reg] << endl;
+void print_register(const char* name, T reg, gregset_t *gregset) {
+    write(STDOUT_FILENO, name, strlen(name));
+    write(STDOUT_FILENO, ": ", 2);
+    print_hex_num((*gregset)[reg], sizeof(void *) * 2);
+    write(STDOUT_FILENO, "\n", 1);
+}
+
+void print_er(const char *str) {
+    write(STDERR_FILENO, str, strlen(str));
+    write(STDERR_FILENO, "\n", 1);
+}
+
+void print(const char *str) {
+    write(STDOUT_FILENO, str, strlen(str));
+    write(STDOUT_FILENO, "\n", 1);
 }
 
 static constexpr size_t radius = 32;
@@ -34,66 +54,59 @@ static const char *l_ar = "-->";
 static const char *r_ar = "<--";
 
 static void handler(int sig, siginfo_t *info, void *ucontext) {
-    cout << "SIGSEGV caught" << endl;
-    cout << "Memory location which caused fault: " << info->si_addr << endl;
-//    print_register("RAX", )
+    print("SIGSEGV caught");
     auto *context = (ucontext_t *) ucontext;
-    gregset = &context->uc_mcontext.gregs;
-    print_register("RAX", REG_RAX);
-    print_register("RBX", REG_RBX);
-    print_register("RCX", REG_RCX);
-    print_register("RDX", REG_RDX);
-    print_register("RBP", REG_RBP);
-    print_register("RSP", REG_RSP);
-    print_register("RSI", REG_RSI);
-    print_register("RDI", REG_RDI);
-    print_register("R8", REG_R8);
-    print_register("R9", REG_R9);
-    print_register("R10", REG_R10);
-    print_register("R11", REG_R11);
-    print_register("R12", REG_R12);
-    print_register("R13", REG_R13);
-    print_register("R14", REG_R14);
-    print_register("R15", REG_R15);
+    gregset_t* gregset= &context->uc_mcontext.gregs;
+    print_register("RAX", REG_RAX, gregset);
+    print_register("RBX", REG_RBX, gregset);
+    print_register("RCX", REG_RCX, gregset);
+    print_register("RDX", REG_RDX, gregset);
+    print_register("RBP", REG_RBP, gregset);
+    print_register("RSP", REG_RSP, gregset);
+    print_register("RSI", REG_RSI, gregset);
+    print_register("RDI", REG_RDI, gregset);
+    print_register("R8", REG_R8, gregset);
+    print_register("R9", REG_R9, gregset);
+    print_register("R10", REG_R10, gregset);
+    print_register("R11", REG_R11, gregset);
+    print_register("R12", REG_R12, gregset);
+    print_register("R13", REG_R13, gregset);
+    print_register("R14", REG_R14, gregset);
+    print_register("R15", REG_R15, gregset);
     int pipefd[2];
     if (pipe(pipefd) == -1) {
-        cerr << "Pipe error: " << strerror(errno) << endl;
-        cerr << "Cannot dump memory" << endl;
+        print_er("Pipe error");
+        print_er("Cannot dump memory");
         exit(EXIT_FAILURE);
     }
     cout << "Memory dump:" << endl;
     auto adr = (size_t) info->si_addr;
-    auto ptr = (char*)info->si_addr;
+    auto ptr = (char *) info->si_addr;
     auto lower = (char *) (adr >= radius ? adr - radius : 0);
     auto upper = (char *) (std::numeric_limits<size_t>::max() - radius >= adr ?
                            adr + radius :
                            std::numeric_limits<size_t>::max());
     for (auto i = lower; i < upper; ++i) {
         if (i == ptr) {
-            write(1, l_ar, strlen(l_ar));
+            write(STDOUT_FILENO, l_ar, strlen(l_ar));
         }
         if (write(pipefd[1], i, 1) != -1) {
-            auto s_stream = std::ostringstream();
-            s_stream << "0x" << std::setfill('0') << std::setw(2) << std::hex
-                     << (unsigned int) (unsigned char) *i;
-            string buf(s_stream.str());
-            write(1, buf.data(), buf.size());
-
+            print_hex_num(*i, 2);
         } else {
             if (errno == EFAULT) {
-                write(1, no_permission, strlen(no_permission));
+                write(STDOUT_FILENO, no_permission, strlen(no_permission));
             } else {
-                write(1, undefined, strlen(undefined));
+                write(STDOUT_FILENO, undefined, strlen(undefined));
             }
         }
         if (i == ptr) {
-            write(1, r_ar, strlen(r_ar));
+            write(STDOUT_FILENO, r_ar, strlen(r_ar));
         }
         if (i != upper) {
-            write(1, delimeter, strlen(delimeter));
+            write(STDOUT_FILENO, delimeter, strlen(delimeter));
         }
         if ((i - lower) % 16 == 15) {
-            write(1, "\n", 1);
+            write(STDOUT_FILENO, "\n", 1);
         }
     }
     exit(EXIT_FAILURE);
@@ -111,9 +124,9 @@ int main(int argc, char *argv[]) {
     }
     //do not expect any errors from sys calls))))
     int pagesize = sysconf(_SC_PAGE_SIZE); //4096 expected
-    char* buffer = (char*)memalign(pagesize, 4 * pagesize);
+    char *buffer = (char *) memalign(pagesize, 4 * pagesize);
     for (int i = 0; i < 4 * pagesize; ++i) {
-        buffer[i] = i%256;
+        buffer[i] = i % 256;
     }
     mprotect(buffer + pagesize, pagesize, PROT_NONE);
     buffer[4097] = 'h';
