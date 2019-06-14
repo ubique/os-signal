80d2b55b592ceb4b87a7c5c5c56453f00c6d82f0
Andrei Filipchyk <awidokmail@gmail.com>
2019-06-14 12:25:14 +0300

    fix errno recovery

--- main.cpp
+++ main.cpp
@@ -55,7 +55,7 @@ void to_str(char* buffer, int value) {
     buffer[start + lg] = '\0';
 }
 
-void write_to_stderr(const char* pointer) {
+void write_to_stderr(const char* pointer, int old_errno) {
     ssize_t result;
     size_t len = strlen(pointer);
     while (len && (result = write(STDERR_FILENO, pointer, len))) {
@@ -63,6 +63,7 @@ void write_to_stderr(const char* pointer) {
             continue;
         }
         if (result < 0) {
+            errno = old_errno;
             exit(EXIT_FAILURE);
         }
         len -= result;
@@ -123,30 +124,30 @@ const char* get_register_name(greg_t reg) {
     }
 }
 
-void print_error(const char* message, char* buffer) {
-    write_to_stderr(message);
-    write_to_stderr(": error number = ");
+void print_error(const char* message, char* buffer, int old_errno) {
+    write_to_stderr(message, old_errno);
+    write_to_stderr(": error number = ", old_errno);
     to_str(buffer, errno);
-    write_to_stderr(buffer + 1);
-    write_to_stderr("\n");
+    write_to_stderr(buffer + 1, old_errno);
+    write_to_stderr("\n", old_errno);
 }
 
 
 void sig_handler(int sig, siginfo_t* info, void* ucontext) {
     int old_errno = errno;
-    write_to_stderr("Segmentation fault at ");
+    write_to_stderr("Segmentation fault at ", old_errno);
     char buffer[1024];
     to_hex(buffer, (size_t)info->si_addr, 8);
-    write_to_stderr(buffer);
-    write_to_stderr("\nRegisters:\n");
+    write_to_stderr(buffer, old_errno);
+    write_to_stderr("\nRegisters:\n", old_errno);
     for (size_t i = 0; i < __NGREG; i++) {
         const char * reg = get_register_name(i);
         if (std::strcmp(reg, "") != 0) {
-            write_to_stderr(reg);
-            write_to_stderr("\t=\t");
+            write_to_stderr(reg, old_errno);
+            write_to_stderr("\t=\t", old_errno);
             to_hex(buffer, ((ucontext_t*)ucontext)->uc_mcontext.gregs[i], 8);
-            write_to_stderr(buffer);
-            write_to_stderr("\n");
+            write_to_stderr(buffer, old_errno);
+            write_to_stderr("\n", old_errno);
         }
     }
     if (info->si_addr != nullptr) {
@@ -158,10 +159,10 @@ void sig_handler(int sig, siginfo_t* info, void* ucontext) {
         if ((size_t)info->si_addr < std::numeric_limits<size_t>::max() - 64) {
             finish = (size_t)info->si_addr + 64;
         }
-        write_to_stderr("Memory dump:\n");
+        write_to_stderr("Memory dump:\n", old_errno);
         int fd[2];
         if (pipe(fd) < 0) {
-            print_error("Can't create pipe", buffer);
+            print_error("Can't create pipe", buffer, old_errno);
             errno = old_errno;
             exit(EXIT_FAILURE);
         }
@@ -169,24 +170,24 @@ void sig_handler(int sig, siginfo_t* info, void* ucontext) {
         for(char* pos = (char*)start; pos != (char*)finish; pos++, j++) {
             if (j % 8 == 0) {
                 to_str(buffer, (int64_t)pos - (int64_t)info->si_addr);
-                write_to_stderr(buffer);
-                write_to_stderr(":\t");
+                write_to_stderr(buffer, old_errno);
+                write_to_stderr(":\t", old_errno);
             }
             std::strcpy(buffer, "????");
             if (write(fd[1], pos, 1) >= 0) {
                to_hex(buffer, (size_t)(*pos) & 0xff, 1);
             }
-            write_to_stderr(buffer);
-            write_to_stderr(" ");
+            write_to_stderr(buffer, old_errno);
+            write_to_stderr(" ", old_errno);
             if (j % 8 == 7) {
-                write_to_stderr("\n");
+                write_to_stderr("\n", old_errno);
             }
         }
         if (close(fd[0]) < 0) {
-            print_error("Can't close pipe", buffer);
+            print_error("Can't close pipe", buffer, old_errno);
         }
         if (close(fd[1]) < 0) {
-            print_error("Can't close pipe", buffer);
+            print_error("Can't close pipe", buffer, old_errno);
         }
     }
     errno = old_errno;
