c67f166d45f95a0eee5d3dcdb33b8b51717b3b93
Damtev <damtev@yandex.ru>
2019-06-14 22:08:24 +0300

    Using async-safe fucntions

--- main.cpp
+++ main.cpp
@@ -1,90 +1,102 @@
-#include <iostream>
+#include <cstdio>
 #include <signal.h>
+#include <unistd.h>
 #include <sys/ucontext.h>
-#include <setjmp.h>
 #include <cstring>
 #include <vector>
-#include <limits>
+#include <cstdint>
+#include <cstdlib>
 
-static const std::vector<std::pair<std::string, greg_t>> REGISTERS = {
-        {"R8",      REG_R8},
-        {"R9",      REG_R9},
-        {"R10",     REG_R10},
-        {"R11",     REG_R11},
-        {"R12",     REG_R12},
-        {"R13",     REG_R13},
-        {"R14",     REG_R14},
-        {"R15",     REG_R15},
-        {"RDI",     REG_RDI},
-        {"RSI",     REG_RSI},
-        {"RBP",     REG_RBP},
-        {"RBX",     REG_RBX},
-        {"RDX",     REG_RDX},
-        {"RAX",     REG_RAX},
-        {"RCX",     REG_RCX},
-        {"RSP",     REG_RSP},
-        {"RIP",     REG_RIP},
-        {"EFL",     REG_EFL},
-        {"CSGSFS",  REG_CSGSFS},
-        {"ERR",     REG_ERR},
-        {"TRAPNO",  REG_TRAPNO},
-        {"OLDMASK", REG_OLDMASK},
-        {"CR2",     REG_CR2}
-};
+#define RANGE 64
 
-static const size_t RANGE = 25;
-static jmp_buf jmpBuf;
-
-void jump_handler(int signo, siginfo_t *siginfo, void *ucontext) {
-    siglongjmp(jmpBuf, 1);
+void write_char(uint8_t ch) {
+    char c = ch < 10 ? '0' + ch : 'A' + ch - 10;
+    write(1, &c, 1);
 }
 
-void dump_memory(const char *addr) {
-    std::cout << "Dumping memory: " << std::endl;
+void write_byte(uint8_t byte) {
+    write_char(byte / 16);
+    write_char(byte % 16);
+}
 
-    size_t point = reinterpret_cast<size_t>(addr);
-    size_t left = (point > RANGE ? point - RANGE : 0);
-    size_t max = std::numeric_limits<size_t>::max();
-    size_t right = (max - point < RANGE ? max : point + RANGE);
-
-    for (size_t i = left; i < right; i += sizeof(char)) {
-        std::cout << "\t";
-
-        sigset_t sigset;
-        sigemptyset(&sigset);
-        sigaddset(&sigset, SIGSEGV);
-        sigprocmask(SIG_UNBLOCK, &sigset, nullptr);
-
-        struct sigaction action;
-        action.sa_flags = SA_SIGINFO;
-        action.sa_sigaction = jump_handler;
-
-        if (sigaction(SIGSEGV, &action, nullptr) == -1) {
-            perror("sigaction");
-            exit(EXIT_FAILURE);
-        }
-        if (setjmp(jmpBuf) != 0) {
-            std::cout << "Can not dump" << std::endl;
-        } else {
-            std::cout << "0x" << std::hex << (int) *reinterpret_cast<char *>(i) << std::endl;
-        }
+void write_num(uint64_t num) {
+    for (int i = 7; i >= 0; --i) {
+        write_byte((num >> (i * 8)) & 0xFF);
     }
+}
+
+void write_str(const char *str) {
+    write(1, str, strlen(str));
 }
 
 void sigsegv_handler(int signo, siginfo_t *siginfo, void *ucontext) {
     if (siginfo->si_signo == SIGSEGV) {
-        std::cout << "SIGSEGV was sent" << std::endl;
-        std::cout << "Address: " << (siginfo->si_addr == nullptr ? "NULL" : siginfo->si_addr)  << std::endl;
+        const std::vector<std::pair<const char*, uint64_t >> registers = {
+                {"R8",      REG_R8},
+                {"R9",      REG_R9},
+                {"R10",     REG_R10},
+                {"R11",     REG_R11},
+                {"R12",     REG_R12},
+                {"R13",     REG_R13},
+                {"R14",     REG_R14},
+                {"R15",     REG_R15},
+                {"RDI",     REG_RDI},
+                {"RSI",     REG_RSI},
+                {"RBP",     REG_RBP},
+                {"RBX",     REG_RBX},
+                {"RDX",     REG_RDX},
+                {"RAX",     REG_RAX},
+                {"RCX",     REG_RCX},
+                {"RSP",     REG_RSP},
+                {"RIP",     REG_RIP},
+                {"EFL",     REG_EFL},
+                {"CSGSFS",  REG_CSGSFS},
+                {"ERR",     REG_ERR},
+                {"TRAPNO",  REG_TRAPNO},
+                {"OLDMASK", REG_OLDMASK},
+                {"CR2",     REG_CR2}
+        };
 
+        write_str("REGISTERS:\n");
         auto *context = static_cast<ucontext_t *>(ucontext);
-        std::cout << "Registers: " << std::endl;
-        for (const auto &reg : REGISTERS) {
-            std::cout << "\t";
-            std::cout << reg.first << " : " << std::hex << "0x" << context->uc_mcontext.gregs[reg.second] << std::endl;
+        for (const auto &reg : registers) {
+            write_str(" ");
+            write_str(reg.first);
+            write_str(" = ");
+            write_num(context->uc_mcontext.gregs[reg.second]);
+            write_str("\n");
         }
 
-        if (siginfo->si_addr != nullptr) {
-            dump_memory(static_cast<char *>(siginfo->si_addr));
+        write_str("MEMORY:\n");
+        int pipefd[2];
+        if (pipe(pipefd) == -1) {
+            write_str("Can not dump\n");
+            exit(EXIT_FAILURE);
+        }
+
+        char *addr = static_cast<char *>(siginfo->si_addr);
+        for (int i = -RANGE; i < RANGE; ++i) {
+            if (i == 0) {
+                write_str("[");
+            } else {
+                write_str(" ");
+            }
+            uint8_t value;
+            if (write(pipefd[1], addr + i, 1) == -1 || read(pipefd[0], &value, 1) == -1) {
+                write_str("?");
+            } else {
+                write_byte(value);
+            }
+            if (i == 0) {
+                write_str("]");
+            } else {
+                write_str(" ");
+            }
+
+            if ((i + 1) % 8 == 0) {
+                write_str("\n");
+            }
+
         }
     }
 
