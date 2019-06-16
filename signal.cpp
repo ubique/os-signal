0cc463cf7b3a06fb16f839ef88ce9e71f3aa19d4
SP4RK <djfungamer@gmail.com>
2019-06-16 16:33:26 +0300

    minor changes

--- signal.cpp
+++ signal.cpp
@@ -5,87 +5,93 @@
 #include <iostream>
 #include <ucontext.h>
 #include <cmath>
-#include <algorithm>
 #include <climits>
 #include <sys/ucontext.h>
-#include <csetjmp>
-#include <vector>
 
 
 static const int MEMORY_DUMP_RANGE = 20;
-const std::vector<std::string> registers = {
-        "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
-        "RDI", "RSI","RBP", "RBX", "RDX", "RAX", "RCX", "RSP", "RIP",
-        "EFL", "CSGSFS", "ERR", "TRAPNO", "OLDMASK", "CR2"};
-static jmp_buf jmpBuf;
+const char* registers[23] = {"R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
+    "RDI", "RSI", "RBP", "RBX", "RDX", "RAX", "RCX", "RSP", "RIP", "EFL", "CSGSFS",
+    "ERR", "TRAPNO", "OLDMASK", "CR2"};
 
-void sigsegvAddressHandler(int signum, siginfo_t* siginfo, void* context) {
-    if (siginfo->si_signo == SIGSEGV) {
-        siglongjmp(jmpBuf, 1);
+void writeByte(uint8_t byte) {
+    byte first = byte >> (unsigned) 4;
+    byte second = byte & (unsigned) 0xF;
+    char firstChar = first + (first < 10 ? '0' : 'A' - 10);
+    char secondChar = second + (second < 10 ? '0' : 'A' - 10);
+    write(1, &firstChar, 1);
+    write(1, &secondChar, 1);
+}
+
+void writeSafe(uint64_t val) {
+    for (int i = 7; i >= 0; --i) {
+        writeByte(0xFF & (val >> (8 * i)));
     }
 }
 
+void writeSafe(const char* string) {
+    write(1, string, strlen(string));
+}
+
+void writeError(const char* message) {
+    write(2, message, strlen(message));
+}
+
 void dumpMemory(void* address) {
-    std::cout << "\nMemory dump: " << "\n";
-    const size_t step = sizeof(char);
-    const long long from = std::max(0LL, (long long) ((char*) address - MEMORY_DUMP_RANGE * step));
-    const long long to = std::min(LONG_LONG_MAX, (long long) ((char*) address + MEMORY_DUMP_RANGE * step));
-
-    for (long long addr = from; addr < to; addr += step) {
-        sigset_t setSignal;
-        sigemptyset(&setSignal);
-        sigaddset(&setSignal, SIGSEGV);
-        sigprocmask(SIG_UNBLOCK, &setSignal, nullptr);
-
-        struct sigaction sigAction{};
-        sigAction.sa_sigaction = sigsegvAddressHandler;
-        sigAction.sa_flags = SA_SIGINFO;
-
-        if (sigaction(SIGSEGV, &sigAction, nullptr) < 0) {
-            std::cerr << "Signal action failed: " << strerror(errno) << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        const char* p = (const char*)addr;
-        std::cout << "0x" << addr;
-        if (setjmp(jmpBuf) != 0) {
-            std::cout << " Couldn't dump\n";
+    writeSafe("\nMemory dump:\n";)
+    int p[2];
+    if (pipe(p) == -1) {
+        writeError("Error while creating pipe");
+        writeError(strerror(errno));
+        _exit(EXIT_FAILURE);
+    }
+    for (long long i = -MEMORY_DUMP_RANGE; i <= MEMORY_DUMP_RANGE; i++) {
+        void* addr = address + i;
+        writeSafe((uint64_t) addr);
+        writeSafe(": ");
+        uint8_t val;
+        if (write(p[1], addr, 1) == -1 || read(p[0], &val, 1) == -1) {
+            writeSafe("Couldn't dump");
         } else {
-            std::cout  << std::hex << +*reinterpret_cast<char*>(addr) << " " << (int)p[0] <<"\n";
+            writeByte(val);
         }
     }
 }
 
 void dumpRegisters(ucontext_t* ucontext) {
-    std::cout << "\nGeneral purposes registers:\n";
+    safeWrite("\nGeneral purposes registers:\n");
     greg_t* gregs = ucontext->uc_mcontext.gregs;
     for (greg_t curReg = REG_R8; curReg != NGREG; curReg++) {
-        std::cout << " " << registers[curReg] << ": 0x" << ucontext->uc_mcontext.gregs[curReg] << "\n";
+        writeSafe(registers[curReg]);
+        writeSafe(": 0x");
+        writeSafe(context->uc_mcontext.gregs[curReg]);
+        writeSafe("\n");
     }
 }
 
 void sigsegvHandler(int signo, siginfo_t* siginfo, void* context) {
     if (siginfo->si_signo == SIGSEGV) {
-        std::cout << "Signal aborted: " << strsignal(signo) << "\n";
+        writeSafe("Signal aborted: ");
+        writeSafe((uint64_t)siginfo->si_signo);
+        writeSafe("\n");
         switch (siginfo->si_code) {
             case SEGV_MAPERR: {
-                std::cout << "\nCause: Address is not mapped to object" << "\n";
+                writeSafe("\nCause: Address is not mapped to object\n");
                 break;
             }
             case SEGV_ACCERR: {
-                std::cout << "\nCause: Invalid permission for mapped object" << "\n";
+                writeSafe("\nCause: Invalid permission for mapped object\n");
                 break;
             }
             default:
-                std::cout << "\nUnsupported code: " << siginfo->si_code << std::endl;
+                writeSafe("\nUnsupported code:");
+                writeSafe(siginfo->si_code);
+                writeSafe("\n");
         }
-
-        auto address = reinterpret_cast<intptr_t>(siginfo->si_addr);
-        std::cout << "Address: 0x" << std::hex << address << "\n";
-
         dumpRegisters(reinterpret_cast<ucontext_t*> (context));
         dumpMemory(siginfo->si_addr);
     }
-    exit(EXIT_FAILURE);
+    _exit(EXIT_FAILURE);
 }
 
 void test1() {
@@ -99,15 +105,16 @@ void test2() {
 }
 
 int main(int argc, char **argv) {
-    struct sigaction action;
+    struct sigaction action{};
     memset(&action, 0, sizeof(action));
     action.sa_sigaction = sigsegvHandler;
     action.sa_flags = SA_SIGINFO;
     if (sigaction(SIGSEGV, &action, nullptr) < 0) {
-        std::cerr << "Sigaction failed: " << strerror(errno) << std::endl;
-        exit(EXIT_FAILURE);
+        writeError("Sigaction failed\n");
+        writeError(strerror(errno));
+        _exit(EXIT_FAILURE);
     }
 //    test1();
     test2();
-    exit(EXIT_SUCCESS);
+    _exit(EXIT_SUCCESS);
 }
